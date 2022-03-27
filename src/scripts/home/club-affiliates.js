f5dc9e7be1a2d4970120c231208aa9769de666c6
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-03-27 17:00:00 +0100

    Finished nav and footer for home site

--- src/scripts/home/club-affiliates.js
+++ src/scripts/home/club-affiliates.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $.getJSON("../../../json/Club.json", function(data){
-        $.each(data, function (key, value){
+    $.getJSON("../../../json/affiliate-clubs.json", function (data) {
+        $.each(data, function (key, value) {
             $("#affiliate-club").append("<img class = 'fphoto-cancha' src='" + value.URL + "'" + "alt='imagen cancha'>");
             $("#affiliate-club").append("<p>" + value.Nombre + "</p>");
             $("#affiliate-club").append("<p>" + value.Direccion + "</p>");
