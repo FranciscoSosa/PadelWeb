ebb22fb67e2ffd5c2524ff01c19b4b40a19c2f6a
unknown <louminxi7@gmail.com>
2022-03-27 15:38:11 +0100

    started responsive design of home site

---
+++ src/scripts/home/Club.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $.getJSON("../../../json/Club.json", function(data){
+        $.each(data, function (key, value){
+            $("#affiliate-club").append("<img class = 'fphoto-cancha' src='" + value.URL + "'" + "alt='imagen cancha'>");
+            $("#affiliate-club").append("<p>" + value.Nombre + "</p>");
+            $("#affiliate-club").append("<p>" + value.Direccion + "</p>");
+            $("#affiliate-club").append("<p>" + value.Descripcion + "</p>");
+            $("#affiliate-club").append("<hr>");
+        });
+    });
+});
