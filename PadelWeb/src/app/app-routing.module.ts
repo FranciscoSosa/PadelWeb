77d3a918a8cc0645801ea04e80f8337bb501bc36
Illera1999 <joseill1414@gmail.com>
2022-04-06 10:09:11 +0100

    user-header-visual

--- PadelWeb/src/app/app-routing.module.ts
+++ PadelWeb/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserHeaderComponent } from './Components/UserHeader/user-header/user-header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "header",
+    component: UserHeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- PadelWeb/src/app/app.module.ts
+++ PadelWeb/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserHeaderComponent } from './Components/UserHeader/user-header/user-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserHeaderComponent
   ],
   imports: [
     BrowserModule,
