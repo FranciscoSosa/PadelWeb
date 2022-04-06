0992d86e177a95ec0806c2759354d29c07a6c4f2
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-04-06 09:30:27 +0100

    User edit profile on progress, ADDED STYLES.CSS TO ANGULAR PROJECT

--- PadelWeb/src/app/app-routing.module.ts
+++ PadelWeb/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserEditProfileComponent} from "./user-edit-profile/user-edit-profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "user-edit-profile",
+    component: UserEditProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- PadelWeb/src/app/app.module.ts
+++ PadelWeb/src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {UserEditProfileComponent} from './user-edit-profile/user-edit-profile.component';
+import {FormsModule} from "@angular/forms";
+import {UserAsideComponent} from './user-aside/user-aside.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserEditProfileComponent,
+    UserAsideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
