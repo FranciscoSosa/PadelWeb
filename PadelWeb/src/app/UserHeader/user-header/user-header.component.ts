5b4ecc8b9ec0fa8b17bbc24744ea510b2a200c15
Illera1999 <joseill1414@gmail.com>
2022-05-06 02:29:57 +0100

    Without Firebase

--- PadelWeb/src/app/UserHeader/user-header/user-header.component.ts
+++ PadelWeb/src/app/UserHeader/user-header/user-header.component.ts
@@ -6,40 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-header.component.css']
 })
 export class UserHeaderComponent implements OnInit {
-
-  element: HTMLElement|null;
-  navmenu: HTMLElement|null;
-
+  show: boolean;
 
   constructor() {
-    this.element = null;
-    this.navmenu = null;
+    this.show=false
   }
 
   ngOnInit(): void {
   }
 
-  dropdown(): void {
-
-    this.element = document.getElementById('menu_principal');
-    this.navmenu = document.getElementById('nav_principal');
-    
-    let i = this.element!.getAttribute("value");
-
-    if (i === "0") {
-        this.navmenu!.setAttribute("class", "nav container-fluid");
-        this.navmenu!.classList.remove("col-5");
-        this.navmenu!.classList.remove("col-md-7");
-        this.element!.classList.remove("d-none");
-        this.element!.classList.remove("d-md-flex");
-        this.element!.setAttribute("value", "1");
-    } else {
-        this.navmenu!.classList.add("col-5");
-        this.navmenu!.classList.add("col-md-7");
-        this.element!.classList.add("d-none");
-        this.element!.classList.add("d-md-flex");
-        this.element!.setAttribute("value", "0");
+  showNav(){
+    if(this.show === false){
+      this.show = true;
+    }else if(this.show === true){
+      this.show = false;
     }
-    
   }
+
 }
