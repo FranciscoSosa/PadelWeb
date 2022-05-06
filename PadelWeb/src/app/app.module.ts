b88364dbc3bf3f3d1d796ba6ae757ae00a09ebdb
Illera1999 <joseill1414@gmail.com>
2022-05-06 02:45:03 +0100

    merge to connect with firebase correct

--- PadelWeb/src/app/app.module.ts
+++ PadelWeb/src/app/app.module.ts
@@ -5,31 +5,26 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {UserEditProfileComponent} from './user-edit-profile/user-edit-profile.component';
 import {FormsModule} from "@angular/forms";
-<<<<<<< HEAD
-import {UserAsideComponent} from './user-aside/user-aside.component';
 import { UserHeaderComponent } from './UserHeader/user-header/user-header.component';
-=======
-import {LoggedAsideComponent} from './logged-aside/logged-aside.component';
->>>>>>> connect-to-firebase
+
 import {HttpClientModule} from "@angular/common/http";
-import { PendingEventsComponent } from './pending-events/pending-events.component';
+
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 import { environment } from 'src/environments/environment';
+
+import { LoggedAsideComponent } from './logged-aside/logged-aside.component';
+import { PendingEventsComponent } from './pending-events/pending-events.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserEditProfileComponent,
-<<<<<<< HEAD
-    UserAsideComponent,
-    UserHeaderComponent
-=======
+    UserHeaderComponent,
     PendingEventsComponent,
     LoggedAsideComponent
->>>>>>> connect-to-firebase
   ],
   imports: [
     BrowserModule,
