71c8b0914e6e130ed1a0987d4c019aebf5d1ee5a
dev <FranciscoSosa>
2022-05-06 17:58:08 +0100

    Added firebase services

---
+++ PadelWeb/src/app/services/club.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Club } from '../types/club';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClubService {
+
+  private dbPath='Club';
+
+  constructor(private db: AngularFirestore) { }
+
+  getClubByEmail(email: string){
+    return this.db.collection<Club>(this.dbPath, ref => ref.where('email', '==', email)).valueChanges({idField: 'id'});;
+  }
+
+  createClub(club: Club){
+    return this.db.collection<Club>(this.dbPath).add({...club});
+  }
+
+  updateClub(id: string, data: any){
+    return this.db.collection<Club>(this.dbPath).doc(id).update(data);
+  }
+
+  deleteClub(id: string){
+    return this.db.collection<Club>(this.dbPath).doc(id).delete();
+  }
+}
