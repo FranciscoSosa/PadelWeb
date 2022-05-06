1d1c280bc993f035010d050f62ed9d0a9875bd48
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-05-06 12:30:07 +0100

    Fixed merge issue

--- PadelWeb/src/app/user-edit-profile/user-edit-profile.component.ts
+++ PadelWeb/src/app/user-edit-profile/user-edit-profile.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-types-edit-profile',
   templateUrl: './user-edit-profile.component.html',
-  styleUrls: ['../../styles.css'],
+  styleUrls: ['user-edit-profile.component.css'],
   host: {
     class: 'content-section ml-auto mr-auto col-lg-9 col-10'
   }
