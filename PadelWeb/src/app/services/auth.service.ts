7778bf2ef30bd7d5efa0cb29a8e951358b5c00a5
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-05-06 02:37:15 +0100

    Implemented authentication service

---
+++ PadelWeb/src/app/services/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Observable, of, switchMap } from 'rxjs';
+import { User } from '../types/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  _user!: Observable<User | undefined>;
+
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore)
+  {
+    this._user = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if(user){
+          return this.afs.doc<User>(`User/${user.uid}`).valueChanges();
+        } else {
+          return of(undefined);
+        }
+      })
+    );
+  }
+
+  async signUp(email: string, password: string) {
+    try {
+      const credentials = await this.afAuth.createUserWithEmailAndPassword(email, password);
+
+      if(credentials.user)
+        this.updateUser(credentials.user);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async signIn(email: string, password: string) {
+    await this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  private updateUser({uid, email}: firebase.default.User) {
+    const userRef = this.afs.doc<User>(`User/${uid}`)
+    const data: User = {
+      uid,
+      email: email!,
+      playedGames: 1,
+      wonGames: 1
+    }
+
+    userRef.set(data, {merge: true})
+  }
+
+  async signOut() {
+    await this.afAuth.signOut();
+  }
+}
--- PadelWeb/src/app/types/user.ts
+++ PadelWeb/src/app/types/user.ts
@@ -2,15 +2,16 @@ import {Tournament} from "./tournament";
 import {Match} from "./match";
 
 export interface User {
-  name: string,
-  surname: string,
+  uid: string,
   email: string,
-  img: string,
+  name?: string,
+  surname?: string,
+  img?: string,
   playedGames: number,
   wonGames: number,
-  rank: string,
-  points: number
-  history: string[],
-  matches: Match[],
-  tournaments: Tournament[]
+  rank?: string,
+  points?: number
+  history?: string[],
+  matches?: Match[],
+  tournaments?: Tournament[]
 }
