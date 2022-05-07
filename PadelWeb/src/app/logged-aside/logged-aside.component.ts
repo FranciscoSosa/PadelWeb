eb0482fe3dbc84992618412cf8601c974d9a452a
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-05-08 00:16:05 +0100

    Modified logged-aside to use firebase

--- PadelWeb/src/app/logged-aside/logged-aside.component.ts
+++ PadelWeb/src/app/logged-aside/logged-aside.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../types/user";
 import {HttpClient} from "@angular/common/http";
 import {Club} from "../types/club";
+import {AuthService} from "../services/auth.service";
+import {ClubAuthService} from "../services/club-auth.service";
 
 @Component({
   selector: 'app-logged-aside',
@@ -10,25 +12,27 @@ import {Club} from "../types/club";
 })
 export class LoggedAsideComponent implements OnInit {
 
-  @Input("infoUrl") infoUrl!: string;
+  @Input("isClub") isClub!: boolean;
 
   userInfo!: User;
   clubInfo!: Club;
 
-  constructor(private http: HttpClient) {
+  constructor(private userService: AuthService, private clubService: ClubAuthService) {
   }
 
   ngOnInit(): void {
-    this.http.get<User | Club>(this.infoUrl).subscribe(info => {
-      if (this.isClub(info)) {
-        this.clubInfo = info as Club;
-      } else {
-        this.userInfo = info as User;
-      }
-    });
-  }
-
-  private isClub(info: User | Club): boolean {
-    return 'courts' in info;
+    if(this.isClub) {
+      this.userService.user$.subscribe(user => {
+        if(user) {
+          this.userInfo = user;
+        }
+      });
+    } else {
+      this.clubService.user$.subscribe(club => {
+        if(club) {
+          this.clubInfo = club;
+        }
+      });
+    }
   }
 }
