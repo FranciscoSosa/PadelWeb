24528669637d2dafcb1a04db4a31634c3892671b
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-05-20 11:36:10 +0100

    Added transparent background ion-content

--- PadelWebIonic/src/app/app-routing.module.ts
+++ PadelWebIonic/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {MainUserPage} from "./main-user/main-user.page";
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'main-user',
+    redirectTo: '/main-user/statistics',
     pathMatch: 'full'
   },
   {
