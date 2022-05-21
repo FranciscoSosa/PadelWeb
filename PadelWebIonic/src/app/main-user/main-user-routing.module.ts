7b50a9a29df8458cf0d6790eab748c177aa4784e
Illera1999 <joseill1414@gmail.com>
2022-05-21 18:49:06 +0100

    Change routerlink to href on user-header

--- PadelWebIonic/src/app/main-user/main-user-routing.module.ts
+++ PadelWebIonic/src/app/main-user/main-user-routing.module.ts
@@ -21,25 +21,26 @@ const routes: Routes = [
       {
         path: 'profile',
         component: EditProfileComponent
+      },
+      {
+        path: 'userChooseClub',
+        loadChildren: () => import('./user-choose-day-and-club/user-choose-day-and-club.module').then( m => m.UserChooseDayAndClubPageModule)
+      },
+      {
+        path: 'chooseMatch/:uid',
+        loadChildren: () => import('./user-choose-match/user-choose-match.module').then( m => m.UserChooseMatchPageModule)
+      },
+      {
+        path: 'events',
+        loadChildren: () => import('./user-events/user-events.module').then( m => m.UserEventsPageModule)
+      },
+      {
+        path: 'eventInfo/:uid/:index',
+        loadChildren: () => import('./user-event-info/user-event-info.module').then( m => m.UserEventInfoPageModule)
       }
     ]
   },
-  {
-    path: 'userChooseClub',
-    loadChildren: () => import('./user-choose-day-and-club/user-choose-day-and-club.module').then( m => m.UserChooseDayAndClubPageModule)
-  },
-  {
-    path: 'chooseMatch/:uid',
-    loadChildren: () => import('./user-choose-match/user-choose-match.module').then( m => m.UserChooseMatchPageModule)
-  },
-  {
-    path: 'events',
-    loadChildren: () => import('./user-events/user-events.module').then( m => m.UserEventsPageModule)
-  },
-  {
-    path: 'eventInfo/:uid/:index',
-    loadChildren: () => import('./user-event-info/user-event-info.module').then( m => m.UserEventInfoPageModule)
-  }
+
 ];
 
 @NgModule({
